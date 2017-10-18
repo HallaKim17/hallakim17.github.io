f90dcf36711ee2fe993eca8a6650b1f395ec0003
HallaKim17 <31562932+HallaKim17@users.noreply.github.com>
2017-10-18 13:52:11 +0900

    Update hw2.js

--- ctp431/hw2/hw2.js
+++ ctp431/hw2/hw2.js
@@ -1,12 +1,28 @@
+var song;
+
+function preload() {
+	song = loadSound("mySong.mp3");
+}
+
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(2000, 1600);
+	song = loadSound("mySong.mp3");
+	song.play();
+
+	angleMode(DEGREES);
+	button = createButton('toggle');
+	button.mousePressed(toggleSong);
+	song.play();
+	fft = new p5.FFT(0,256);
 }
 
 function draw() {
 	background(0);
-	stroke(255);
+    var spectrum = amp.analyze();
+
+    console.log(spectrum.length);
+    stroke(255);
 	strokeWeight(4);
 	nofill();
-	ellipse(x, 200, 100, 100);
 }
 
