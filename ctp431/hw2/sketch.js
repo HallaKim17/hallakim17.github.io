8dee1baced568a79c65f6ef059bb29a187a5e346
hallakim <kimhalla@kaist.ac.kr>
2017-10-19 23:48:21 +0900

    started editing p5 code 1

--- ctp431/hw2/sketch.js
+++ ctp431/hw2/sketch.js
@@ -1,14 +1,28 @@
 var song;
+var button;
+
+function toggleSong() {
+	if (song.isPlaying()) {
+		song.pause();
+	} else {
+		song.play();
+	}
+}
 
 function preload() {
 	song = loadSound("mySong.mp3")
 }
 
 function setup() {
-	createCanvas(1000, 700);
+	createCanvas(600, 400);
+	button = createButton('toggle');
+	button.mousePressed(toggleSong);
 	song.play();
+	amp = new p5.Amplitude();
 }
 
 function draw() {
 	background(0);
+	var vol = amp.getLevel();
+	ellipse(300, 200, 200, vol * 300)
 }
