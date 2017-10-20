e25637fc4ab1ace0831681e36603863416f3eb46
hallakim <kimhalla@kaist.ac.kr>
2017-10-20 12:16:28 +0900

    5

--- ctp431/hw2/sketch.js
+++ ctp431/hw2/sketch.js
@@ -15,15 +15,31 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(200, 200);
+	createCanvas(windowWidth, windowHeight, WEBGL);
 	button = createButton('toggle');
 	button.mousePressed(toggleSong);
 	song.play();
 	amp = new p5.Amplitude();
+
+
 }
 
 function draw() {
-	background(0);
+	background(250);
+    rotateY(frameCount * 0.01);
+
+    for(var j=0; j<5; j++){
+    	push();
+    	for(var i=0; i<80; i++){
+    		translate(sin(frameCount * 0.001 + j) * 100, sin(frameCount * 0.001 + j) * 100, i * 0.1);
+    		rotateZ(frameCount * 0.002);
+    		push();
+    		sphere(8,6,4);
+    		pop();
+    	}
+    	pop();
+    }	
+
 	var vol = amp.getLevel();
 	stroke(255);
 	ellipse(100, 100, 100, vol * 100)
